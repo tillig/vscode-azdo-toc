294593352d0546a5d2f7e4033967e683ce11d712
Travis Illig <tillig@paraesthesia.com>
2023-05-19 07:50:50 -0700

    Linting for build time.

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,11 @@
-import type MarkdownIt from "markdown-it";
-import Renderer from "markdown-it/lib/renderer";
-import StateCore from "markdown-it/lib/rules_core/state_core";
+import type MarkdownIt from 'markdown-it';
+import Renderer from 'markdown-it/lib/renderer';
+import StateCore from 'markdown-it/lib/rules_core/state_core';
 
 /**
  * Activates the extension and attaches the Azure DevOps TOC preview to the Markdown preview.
  * @param {vscode.ExtensionContext} context The extension context with which commands may be registered.
+ * @return {function(MarkdownIt): MarkdownIt} A function to execute to add the rules for preview.
  */
 export function activate() {
   return {
@@ -22,24 +23,24 @@ function tocPreviewer(md: MarkdownIt) {
   let documentState: StateCore;
 
   // Register a rule to grab the entire document so we can parse out headings.
-  md.core.ruler.push("vscode-azdo-toc-state", (state) => {
+  md.core.ruler.push('vscode-azdo-toc-state', (state) => {
     documentState = state;
   });
 
   // Register a rule to kick off the TOC renderer when the [[_TOC_]] block is hit.
-  md.core.ruler.push("vscode_azdo_toc", (state) => {
+  md.core.ruler.push('vscode_azdo_toc', (state) => {
     const blockTokens = state.tokens;
     for (let j = 0, l = blockTokens.length; j < l; j++) {
       const blockToken = blockTokens[j];
       if (
-        blockToken.type !== "inline" ||
-        blockToken.content !== "[[_TOC_]]"
+        blockToken.type !== 'inline' ||
+        blockToken.content !== '[[_TOC_]]'
       ) {
         continue;
       }
 
       // Our renderer gets invoked when this token is rendered.
-      blockToken.children = [new state.Token("vscode_azdo_toc", "", 0)];
+      blockToken.children = [new state.Token('vscode_azdo_toc', '', 0)];
     }
   });
 
@@ -52,34 +53,34 @@ function tocPreviewer(md: MarkdownIt) {
     self: Renderer
   ) => {
     const tokens = documentState.tokens;
-    let tocBody = "";
+    let tocBody = '';
     let currentLevel = 0;
     for (let i = 0; i < tokens.length; ++i) {
       const token = tokens[i];
-      if (token.type === "heading_open") {
+      if (token.type === 'heading_open') {
         // Tag is h1 - h6. Parse the current indent level based on the tag number.
         const level = parseInt(token.tag.substring(1));
         if (level > currentLevel) {
           // Indent from current level to target level. If it jumps from H1 to H3, it'll get two indents.
           tocBody += '<ul>'.repeat(level - currentLevel);
-        } else if (level == currentLevel) {
+        } else if (level === currentLevel) {
           // Heading at the same level. End the previous heading so we can start the next one.
-          tocBody += "</li>";
+          tocBody += '</li>';
         } else {
           // Heading at a previous level. End the previous heading and unindent. If it jumps from H3 to H1, it'll get two unindents.
-          tocBody += "</li>" + "</ul>".repeat(currentLevel - level);
+          tocBody += '</li>' + '</ul>'.repeat(currentLevel - level);
         }
 
         // Start the list item for the header.
         tocBody += '<li>';
         currentLevel = level;
       } else if (
-        token.type === "inline" &&
-        tokens[i - 1].type === "heading_open"
+        token.type === 'inline' &&
+        tokens[i - 1].type === 'heading_open'
       ) {
         // This is the tag content.
         tocBody += `<a href="#${slugify(token.content)}">${self.renderInline(
-          token.children!,
+          token.children ?? [],
           options,
           env
         )}</a>`;
@@ -87,13 +88,13 @@ function tocPreviewer(md: MarkdownIt) {
     }
     for (; currentLevel > 0; --currentLevel) {
       // Close any unclosed headers.
-      tocBody += "</li></ul>";
+      tocBody += '</li></ul>';
     }
     return (
       '<div style="border:1px solid;border-color:rgba(var(--palette-neutral-8,234, 234, 234),1);border-radius:4px;display:inline-block;padding:10px 16px 0px 0px;min-width:250px;margin-bottom:14px;">' +
       '<div style="font-weight:600;margin:0 16px 5px 16px;">Contents</div>' +
       tocBody +
-      "</div>"
+      '</div>'
     );
   };
 }
@@ -101,10 +102,10 @@ function tocPreviewer(md: MarkdownIt) {
 /**
  * Converts header text into an anchor link.
  * @param {string} s The header content to convert.
- * @returns The anchor link associated with the header.
+ * @return {string} The anchor link associated with the header.
  */
-function slugify(s: string) {
+function slugify(s: string): string {
   return encodeURIComponent(
-    String(s).trim().toLowerCase().replace(/\s+/g, "-")
+    String(s).trim().toLowerCase().replace(/\s+/g, '-')
   );
 }
